b57feda09aa7574292946a0bad1edbd075ff4119
Kevin Higgs <github@kevinhiggs.com>
2019-03-08 16:48:56 -0500

    update time

--- timer.js
+++ timer.js
@@ -10,7 +10,7 @@ function setProgress (name, ratio) {
 
 function step (timestamp) {
 	var present = new Date()
-	var competition = Date.parse("2019-04-05T20:00:00.000-04:00")
+	var competition = Date.parse("2019-04-19T20:00:00.000-04:00")
 	var delta = (competition - present) / 1000
 	setValue('day', Math.floor(delta / 86400))
 	setProgress('day', delta / 31536000)
