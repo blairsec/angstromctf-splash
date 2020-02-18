477e183e63d4b0469f8d3003289198facc8dedc4
Kevin Higgs <github@kevinhiggs.com>
2020-02-18 17:56:16 -0500

    2020

--- timer.js
+++ timer.js
@@ -10,7 +10,7 @@ function setProgress (name, ratio) {
 
 function step (timestamp) {
 	var present = new Date()
-	var competition = Date.parse("2019-03-13T20:00:00.000-04:00")
+	var competition = Date.parse("2020-03-13T20:00:00.000-04:00")
 	var delta = (competition - present) / 1000
 	delta = delta > 0 ? delta : 0
 	setValue('day', Math.floor(delta / 86400))
